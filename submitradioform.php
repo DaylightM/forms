8bf7eea00de60f5a8214f32ff0cac6b599c6a75a
DaylightM <tigertechuk@gmail.com>
2024-10-13 05:37:22 +0100

    Add files via upload

---
+++ submitradioform.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome your favorite language is: <?php echo $_POST["fav_language"]; ?><br>
+
+
+</body>
+</html>
---
+++ submtcheckform.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Hello your favorite method of travelling is: <?php echo $_POST["vehicle"]; ?><br>
+
+
+</body>
+</html>
